870d92759f7289795c7e0113193ca3cad7176df6
Jose Airton Feitoza <airton.feitoza@midtec.net.br>
2020-04-18 13:47:10 -0300

    Fist Commit

---
+++ app/Http/Controllers/PropertiesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Properties;
+use Illuminate\Http\Request;
+
+class PropertiesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Properties $properties)
+    {
+        //
+    }
+
+    public function edit(Properties $properties)
+    {
+        //
+    }
+
+   public function update(Request $request, Properties $properties)
+    {
+        //
+    }
+
+    public function destroy(Properties $properties)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_04_11_183808_create_user_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->integer('phone');
+            $table->integer('mobile_phone');
+            $table->integer('individual_legal_registration');
+            $table->integer('kind_person');
+            $table->string('photo');
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
