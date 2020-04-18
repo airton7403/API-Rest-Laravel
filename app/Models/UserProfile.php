12c8762079dc91978c20358f5637fc48ab8148b7
José Airton Feitoza <airton.feitza@midtec.net.br>
2020-04-18 23:37:15 +0000

    Model

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class UserProfile extends Model
 {
-    //
+    use Notifiable;
+
+    protected $fillable = [
+        'phone',
+        'mobile_phone',
+        'individual_legal_registration',
+        'kind_person',
+        'photo',
+    ];
+
+    protected $hidden = [
+        'id',
+        'user_id'
+    ];
+
+    protected $casts = [];
 }
