95ee7ab9776e9894be3b9f3306d6cc8c107d9689
José Airton Feitoza <airton.feitza@midtec.net.br>
2020-04-18 23:39:46 +0000

    Criando controller para relaciomento

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function index()
+    {
+        $user = $this->user->with(['profile'])->paginate(10);
+
+        return response()->json($user, 200);
+    }
+}
