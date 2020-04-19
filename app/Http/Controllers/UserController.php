428fedcf52cc3e537cc6588bc9fbbd866f2325bb
Jose Airton Feitoza <airton.feitoza@midtec.net.br>
2020-04-18 23:24:07 -0300

    Limpado core;

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
 {
     private $user;
+    private $product;
 
-    public function __construct(User $user)
+    public function __construct(User $user, Product $product)
     {
         $this->user = $user;
+        $this->product = $product;
     }
 
     public function index()
     {
-        $user = $this->user->with(['profile', 'products'])->paginate(10);
+        $user = $this->user->with(['profile'])->paginate(10);
 
         return response()->json($user, 200);
     }
